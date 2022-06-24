4cdd05693481fd7fe8bfcf66b4650b1672c0cdc7
BeainaT <tania.df@hotmail.it>
2022-06-24 15:33:08 +0200

    added milestone 1 + css debug style

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <?php
-//Prima Milestone:
-//Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+//MILESTONE 1: print disks in DOM using only PHP code
+//include database file
+require_once './server/database.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+        <!-- print disks in HTML -->
+        <?php foreach($database as $key => $album) : ?>
+            <img <?= "src=$album[poster]" ?> />
+            <h3><?= $album['title']?></h3>
+            <h4><?= $album['author']?></h4>
+            <h4><?= $album['genre']?></h4>
+            <h4><?= $album['year']?></h4>
+        <?php endforeach; ?>
+        <!-- /print disks in HTML -->
+    </body>
+
 </html>
