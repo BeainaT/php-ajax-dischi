459331108586a3e1d54eebdafd6ff41cb97af850
BeainaT <tania.df@hotmail.it>
2022-06-25 20:49:13 +0200

    added bonus / new css properties aren't show!!

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        albums: [] //init empty array
+        albums: [], //init empty array
+        select: "",
+        genres: new Set(), // init object as Set constructor to collect one value/type of iterable obj passed
     },
     created() {
         //call axios on created and pass api endpoint
@@ -10,9 +12,29 @@ const app = new Vue ({
         .then((response) => {
             //set albums as api's endpoint response.data
             this.albums = response.data;
+            this.albums.forEach(elm => {
+                //genres'll contain every "genre/type in album" repeated value only once
+                this.genres.add(elm.genre);
+            });
         })
         .catch((error) => {
             console.log(error)
-        })
+        });
+    },
+    methods: {
+        //2nd call to axios passing select parameter (filter setted in php file)
+        filterGenre() {
+            axios.get('http://localhost/php-ajax-dischi/server/api.php', {
+                params: {
+                    genre: this.select
+                }
+            })
+            .then((response) => {
+                this.albums = response.data
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+        },
     }
 });
--- server/api.php
+++ server/api.php
@@ -3,8 +3,23 @@
 
 //include database file
 require_once 'database.php';
+
 //change header type from text in json
 header('content-Type: application/json');
-//print $database after json encoding
-echo json_encode($database)
+
+//
+if(empty($_GET['genre']) || $_GET['genre'] == "all") {
+    //print $database after json encoding
+    echo json_encode($database);
+} else {
+    $filtered = [];
+    //for each album in database
+    foreach($database as $album) {
+        //if (genre in album == select value) new $filtered array will setted as 'old' album array
+        !($album['genre'] == $_GET['genre']) ?: $filtered[] = $album; //this writted condition returns false so condition must be negated
+    }
+    //print filtered array
+    echo json_encode($filtered);
+}
+
 ?>
